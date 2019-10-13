9f6f58e497eb81c66b084d547bb1223da9a62529
Emmanuel Salcido <salcidoe@amdocs.com>
2019-10-12 23:31:17 -0500

    Initial commit

---
+++ src/main/java/com/product/arkhe/com_product_arkhe/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.product.arkhe.com_product_arkhe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/product/arkhe/com_product_arkhe/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.arkhe.com_product_arkhe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
