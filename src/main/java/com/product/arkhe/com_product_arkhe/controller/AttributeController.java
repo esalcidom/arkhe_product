369f3ee5d131382b4c05a7a2312f4bea8c012702
Emmanuel Salcido <salcidoe@amdocs.com>
2019-10-13 23:42:25 -0500

    adding schema and data for H2

--- src/main/java/com/product/arkhe/com_product_arkhe/controller/AttributeController.java
+++ src/main/java/com/product/arkhe/com_product_arkhe/controller/AttributeController.java
@@ -6,7 +6,6 @@ import com.product.arkhe.com_product_arkhe.model.Attribute;
 import com.product.arkhe.com_product_arkhe.service.AttributeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
